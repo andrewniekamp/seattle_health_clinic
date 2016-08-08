4c83f035d13ffc4ebc4f27aeceb44df97dcc1e4d
Andrew Niekamp and Yi Chiang <andrew+yi@>
2016-08-08 15:53:05 -0700

    add form for new visit and routes for new patient and new visit

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -17,8 +17,24 @@ namespace SeattleHealthClinic
       };
 
       Post["/add/patients"] = _ =>{
-        Patients newPatient = new Patient(Request.Form["patient-name"], Request.Form["patient-address"]);
+        Patient newPatient = new Patient(Request.Form["patient-name"], Request.Form["patient-address"]);
         newPatient.Save();
+        return View["success.cshtml"];
+      };
+      Get["/add/visit"] = _ =>{
+        List<Patient> allPatients = Patient.GetAll();
+        //List<Physician> allPhysicians = Physician.GetAll();
+        List<Condition> allConditions = Condition.GetAll();
+        Dictionary<string,object> model = new Dictionary<string,object>{};
+        model.Add("patients",allPatients);
+        //model.Add("physicians", allPhysicians);
+        model.Add("conditions",allConditions);
+        return View["add_visit.cshtml",model];
+      };
+      Post["/add/visit"] = _ =>{
+        //Temporary DoctorId used in constructor below
+        ConditionEval newVisit = new ConditionEval(Request.Form["visit-patient-id"],1, Request.Form["visit-condition-id"],Request.Form["visit-date"]);
+        newVisit.Save();
         return View["success.cshtml"];
       };
     }
