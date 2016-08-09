a75b077ad26c96064795c58bba071bca72a28a50
RubyBe <sidbenavente@hotmail.com>
2016-08-09 10:03:56 -0700

    add payroll class

---
+++ Objects/Payroll.cs
@@ -0,0 +1,220 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SeattleHealthClinic
+{
+  public class Payroll
+  {
+    // properties
+    internal int _id;
+    internal string _payPeriod;
+    internal string _salaryType;
+    internal string _salaryAmount;
+
+    // constructors, getters, setters
+    public Payroll(string payperiod, string salarytype, double salaryamount, int Id = 0)
+    {
+      _payPeriod = payperiod;
+      _salaryType = salarytype;
+      _salaryAmount = salaryamount;
+      _id = Id;
+    }
+    public string GetPayPeriod()
+    {
+      return _payPeriod;
+    }
+    public string GetSalaryType()
+    {
+      return _salaryType;
+    }
+    public string GetSalaryAmount()
+    {
+      return _salaryAmount;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    // other methods
+    // a method to save an employee to the database
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("INSERT INTO employees (employee_name_first, employee_name_last) OUTPUT INSERTED.id VALUES (@EmployeeNameFirst, @EmployeeNameLast);", conn);
+      SqlParameter firstNameParameter = new SqlParameter();
+      firstNameParameter.ParameterName = "@EmployeeNameFirst";
+      firstNameParameter.Value = this.GetFirstName();
+      cmd.Parameters.Add(firstNameParameter);
+      SqlParameter lastNameParameter = new SqlParameter();
+      lastNameParameter.ParameterName = "@EmployeeNameLast";
+      lastNameParameter.Value = this.GetLastName();
+      cmd.Parameters.Add(lastNameParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if(rdr!=null)
+      {
+        rdr.Close();
+      }
+      if(conn!=null)
+      {
+        conn.Close();
+      }
+    }
+    // a method to find an employee using the employee id
+    public Employee Find(int Id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("SELECT * FROM employees WHERE id = @EmployeeId;", conn);
+      SqlParameter employeeIdParameter = new SqlParameter();
+      employeeIdParameter.ParameterName = "@EmployeeId";
+      employeeIdParameter.Value = Id.ToString();
+      cmd.Parameters.Add(employeeIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+      int foundEmployeeId = 0;
+      string foundEmployeeFirstName = null;
+      string foundEmployeeLastName = null;
+      while(rdr.Read())
+      {
+        foundEmployeeId = rdr.GetInt32(0);
+        foundEmployeeFirstName = rdr.GetString(1);
+        foundEmployeeLastName = rdr.GetString(2);
+      }
+      Employee foundEmployee = new Employee(foundEmployeeFirstName, foundEmployeeLastName, foundEmployeeId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundEmployee;
+    }
+    // a method to update the name of an employee
+    public void UpdateName(string newFirst, string newLast)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("UPDATE employees SET employee_name_first = @NewFirst, employee_name_last = @NewLast OUTPUT INSERTED.employee_name_first, INSERTED.employee_name_last WHERE id = @EmployeeId;", conn);
+      SqlParameter newFirstParameter = new SqlParameter();
+      newFirstParameter.ParameterName = "@NewFirst";
+      newFirstParameter.Value = newFirst;
+      cmd.Parameters.Add(newFirstParameter);
+      SqlParameter newLastParameter = new SqlParameter();
+      newLastParameter.ParameterName = "@NewLast";
+      newLastParameter.Value = newLast;
+      cmd.Parameters.Add(newLastParameter);
+      SqlParameter employeeIdParameter = new SqlParameter();
+      employeeIdParameter.ParameterName = "@EmployeeId";
+      employeeIdParameter.Value = this.GetId().ToString();
+      cmd.Parameters.Add(employeeIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+      while(rdr.Read())
+      {
+        this._employee_name_first = rdr.GetString(0);
+        this._employee_name_last = rdr.GetString(1);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    // a method to return a list of all employees table records
+    public static List<Employee> GetAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("SELECT * FROM employees ORDER BY employee_name_last;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+      List<Employee> allEmployees = new List<Employee>{};
+      while(rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string firstName = rdr.GetString(1);
+        string lastName = rdr.GetString(2);
+        Employee newEmployee = new Employee(firstName, lastName, id);
+        allEmployees.Add(newEmployee);
+      }
+      if (rdr !=null)
+      {
+        rdr.Close();
+      }
+      if (conn !=null)
+      {
+        conn.Close();
+      }
+      return allEmployees;
+    }
+    // a method to delete all employees table records
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM employees;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+    // methods which interact with the License class/datatable
+    // a method to add create an association between an employee and a license
+    public void AddLicense(License newLicense)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("INSERT INTO certifications (employee_id, license_id) VALUES (@EmployeeId, @LicenseId);", conn);
+      SqlParameter employeeIdParameter = new SqlParameter();
+      employeeIdParameter.ParameterName = "@EmployeeId";
+      employeeIdParameter.Value = this.GetId();
+      cmd.Parameters.Add(employeeIdParameter);
+      SqlParameter licenseIdParameter = new SqlParameter();
+      licenseIdParameter.ParameterName = "@LicenseId";
+      licenseIdParameter.Value = newLicense.GetId();
+      cmd.Parameters.Add(licenseIdParameter);
+      cmd.ExecuteNonQuery();
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    // a method to get all licenses associated with a particular employee
+    public List<License> GetLicenses()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("SELECT licenses.* FROM employees JOIN certifications ON (employees.id = certifications.employee_id) JOIN licenses ON (certifications.license_id = licenses.id) WHERE employees.id = @EmployeeId;", conn);
+      SqlParameter EmployeeIdParameter = new SqlParameter();
+      EmployeeIdParameter.ParameterName = "@EmployeeId";
+      EmployeeIdParameter.Value = this.GetId().ToString();
+      cmd.Parameters.Add(EmployeeIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+      List<License> licenses = new List<License>{};
+      while(rdr.Read())
+      {
+        int licenseId = rdr.GetInt32(0);
+        string licenseNumber = rdr.GetString(1);
+        string licenseType = rdr.GetString(2);
+        License newLicense = new License(licenseNumber, licenseType, licenseId);
+        licenses.Add(newLicense);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return licenses;
+    }
+  }
+}
