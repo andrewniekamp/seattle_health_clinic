22fce80370a31c7696e7722fa026262302f10680
Andrew Niekamp and Yi Chiang <andrew+yi@>
2016-08-09 09:54:16 -0700

    add patient scheduling class and tests

---
+++ Objects/PatientScheduling.cs
@@ -0,0 +1,231 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace SeattleHealthClinic
+{
+  public class PatientScheduling
+  {
+    private int _id;
+    private int _patientId;
+    private int _doctorId;
+    private string _note;
+    private DateTime _appointmentDate;
+
+    public PatientScheduling (int patientId, int doctorId, string Note, DateTime appointmentDate, int id = 0)
+    {
+      _id = id;
+      _patientId = patientId;
+      _doctorId=doctorId;
+      _note=Note;
+      _appointmentDate = appointmentDate;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public int GetPatientId()
+    {
+      return _patientId;
+    }
+
+    public int GetDoctorId()
+    {
+      return _doctorId;
+    }
+
+    public void SetPatientId(int newPatientId)
+    {
+      _patientId = newPatientId;
+    }
+
+    public string GetNote()
+    {
+      return _note;
+    }
+    public void SetNote(string newNote)
+    {
+      _note = newNote;
+    }
+
+    public string GetPatientSchedulingDate()
+    {
+      return _appointmentDate.ToString("MM/dd/yyyy");
+    }
+    public void SetPatientId(DateTime newPatientSchedulingDate)
+    {
+      _appointmentDate = newPatientSchedulingDate;
+    }
+
+    public override bool Equals(System.Object otherPatientScheduling)
+    {
+      if (!(otherPatientScheduling is PatientScheduling))
+      {
+        return false;
+      }
+      else
+      {
+        PatientScheduling newPatientScheduling = (PatientScheduling) otherPatientScheduling;
+        bool idEquality = this.GetId() == newPatientScheduling.GetId();
+        bool patientIdEquality = this.GetPatientId() == newPatientScheduling.GetPatientId();
+        bool noteEquality = this.GetNote() == newPatientScheduling.GetNote();
+        bool doctorIdEquality = this.GetDoctorId() == newPatientScheduling.GetDoctorId();
+        bool appointmentDateEquality = this.GetPatientSchedulingDate() == newPatientScheduling.GetPatientSchedulingDate();
+
+        return (idEquality && patientIdEquality && noteEquality && appointmentDateEquality&&doctorIdEquality);
+      }
+    }
+
+    public static List<PatientScheduling> GetAll()
+    {
+      List<PatientScheduling> allPatientSchedulings = new List<PatientScheduling>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM patients_scheduling ORDER BY appointment_date;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while (rdr.Read())
+      {
+        int patientSchedulingId = rdr.GetInt32(0);
+        int patientId = rdr.GetInt32(1);
+        int doctorId = rdr.GetInt32(2);
+        string note = rdr.GetString(3);
+        DateTime appointmentDate = rdr.GetDateTime(4);
+
+        PatientScheduling newPatientScheduling = new PatientScheduling(patientId,doctorId,note, appointmentDate, patientSchedulingId);
+        allPatientSchedulings.Add(newPatientScheduling);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allPatientSchedulings;
+    }
+
+
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO patients_scheduling (patient_id, doctor_id, note, appointment_date) OUTPUT INSERTED.id VALUES (@PatientId, @DoctorId,@Note, @PatientSchedulingDate);", conn);
+
+      SqlParameter patientIdParameter = new SqlParameter();
+      patientIdParameter.ParameterName = "@PatientId";
+      patientIdParameter.Value = this.GetPatientId();
+      cmd.Parameters.Add(patientIdParameter);
+
+      SqlParameter noteParameter = new SqlParameter();
+      noteParameter.ParameterName = "@Note";
+      noteParameter.Value = this.GetNote();
+      cmd.Parameters.Add(noteParameter);
+
+      SqlParameter doctorIdParameter = new SqlParameter();
+      doctorIdParameter.ParameterName = "@DoctorId";
+      doctorIdParameter.Value = this.GetDoctorId();
+      cmd.Parameters.Add(doctorIdParameter);
+
+      SqlParameter appointmentDateParameter = new SqlParameter();
+      appointmentDateParameter.ParameterName = "@PatientSchedulingDate";
+      appointmentDateParameter.Value = this.GetPatientSchedulingDate();
+      cmd.Parameters.Add(appointmentDateParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while (rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static PatientScheduling Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM patients_scheduling WHERE id = @PatientSchedulingId;", conn);
+      SqlParameter performanceIdParameter = new SqlParameter();
+      performanceIdParameter.ParameterName = "@PatientSchedulingId";
+      performanceIdParameter.Value = id.ToString();
+
+      cmd.Parameters.Add(performanceIdParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      int foundPatientSchedulingId = 0;
+      int foundPatientId = 0;
+      int founddoctorId = 0;
+      string foundNote = null;
+
+      DateTime foundPatientSchedulingDate = DateTime.MinValue;
+
+      while (rdr.Read())
+      {
+        foundPatientSchedulingId = rdr.GetInt32(0);
+        foundPatientId = rdr.GetInt32(1);
+        founddoctorId = rdr.GetInt32(2);
+        foundNote = rdr.GetString(3);
+        foundPatientSchedulingDate = rdr.GetDateTime(4);
+      }
+
+      PatientScheduling foundPatientScheduling = new PatientScheduling(foundPatientId, founddoctorId,foundNote, foundPatientSchedulingDate, foundPatientSchedulingId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundPatientScheduling;
+    }
+
+    public void Delete()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM patients_scheduling WHERE id = @PatientSchedulingId", conn);
+
+      SqlParameter schedulingIdParameter = new SqlParameter();
+      schedulingIdParameter.ParameterName = "@PatientSchedulingId";
+      schedulingIdParameter.Value = this.GetId();
+
+      cmd.Parameters.Add(schedulingIdParameter);
+
+      cmd.ExecuteNonQuery();
+
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM patients_scheduling;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+  }
+}
---
+++ Tests/PatientSchedulingTest.cs
@@ -0,0 +1,111 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SeattleHealthClinic
+{
+  public class PatientSchedulingTest : IDisposable
+  {
+    public PatientSchedulingTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=seattle_health_clinic_test;Integrated Security=SSPI;";
+    }
+    public void Dispose()
+    {
+      Patient.DeleteAll();
+      Condition.DeleteAll();
+      PatientScheduling.DeleteAll();
+      PatientScheduling.DeleteAll();
+    }
+
+    [Fact]
+    public void T1_DBEmptyAtFirst()
+    {
+      int result = PatientScheduling.GetAll().Count;
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void T2_Equal_ReturnsTrueIfPatientSchedulingIsSame()
+    {
+      DateTime appointmentDate = new DateTime(2016,08,04);
+
+      PatientScheduling firstPatientScheduling = new PatientScheduling(1, 2, "The patient reports general discomfort in the abdomen.", appointmentDate);
+      PatientScheduling secondPatientScheduling = new PatientScheduling(1, 2, "The patient reports general discomfort in the abdomen.", appointmentDate);
+
+      Assert.Equal(firstPatientScheduling, secondPatientScheduling);
+    }
+
+    [Fact]
+    public void T3_Save_SavesToDB()
+    {
+      Patient testPatient = new Patient("Anderson", "1234 Main Street");
+      testPatient.Save();
+      DateTime appointmentDate = new DateTime(2016,08,04);
+
+      PatientScheduling testPatientScheduling = new PatientScheduling(testPatient.GetId(), 1, "The patient reports general discomfort in the abdomen.", appointmentDate);
+      testPatientScheduling.Save();
+
+      List<PatientScheduling> result = PatientScheduling.GetAll();
+      List<PatientScheduling> testList = new List<PatientScheduling>{testPatientScheduling};
+
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void T4_Save_AssignsIdToPatientScheduling()
+    {
+      DateTime appointmentDate = new DateTime(2016,08,04);
+
+      PatientScheduling testPatientScheduling = new PatientScheduling(1, 2, "The patient reports general discomfort in the abdomen.", appointmentDate);
+      testPatientScheduling.Save();
+
+      PatientScheduling savedPatientScheduling = PatientScheduling.GetAll()[0];
+      int result = savedPatientScheduling.GetId();
+      int testId = testPatientScheduling.GetId();
+
+      Assert.Equal(testId, result);
+    }
+
+    [Fact]
+    public void T5_Find_FindsPatientSchedulingInDB()
+    {
+      DateTime appointmentDate = new DateTime(2016,08,04);
+
+      PatientScheduling testPatientScheduling = new PatientScheduling(1, 2, "The patient reports general discomfort in the abdomen.", appointmentDate);
+      testPatientScheduling.Save();
+
+      PatientScheduling foundPatientScheduling = PatientScheduling.Find(testPatientScheduling.GetId());
+
+      Assert.Equal(testPatientScheduling, foundPatientScheduling);
+    }
+
+
+    [Fact]
+    public void T6_Delete_DeleteRelationshipsInOtherTables()
+    {
+      Patient testPatient = new Patient("Anderson", "1234 Main Street");
+      testPatient.Save();
+
+      Patient testPatient2 = new Patient("And", "1234");
+      testPatient2.Save();
+      DateTime appointmentDate = new DateTime(2016,08,04);
+
+      PatientScheduling testPatientScheduling = new PatientScheduling(testPatient.GetId(),  1, "The patient reports general discomfort in the abdomen.", appointmentDate);
+      testPatientScheduling.Save();
+
+      PatientScheduling testPatientScheduling2 = new PatientScheduling(testPatient2.GetId(),  1, "The patient reports general discomfort in the abdomen.", appointmentDate);
+      testPatientScheduling2.Save();
+
+      testPatientScheduling.Delete();
+
+      List<PatientScheduling> result = PatientScheduling.GetAll();
+      List<PatientScheduling> testList = new List<PatientScheduling>{testPatientScheduling2};
+
+      Assert.Equal(testList, result);
+    }
+
+  }
+}
