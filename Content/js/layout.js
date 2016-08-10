9bab8ef0761963c8cb02432bfffb078cc872ea0c
Steven Tran <steventranucsd@gmail.com>
2016-08-09 23:12:19 -0700

    add refactored code for collapsable panel model

--- Content/js/layout.js
+++ Content/js/layout.js
@@ -1,26 +1,20 @@
 $(document).ready(function() {
-  $("#list-one").click(function(event) {
-    $("#panel-one").slideDown();
-    $("#panel-two").slideUp();
-    $("#panel-three").slideUp();
-    $("#panel-four").slideUp();
-  });
-  $("#list-two").click(function(event) {
-    $("#panel-one").slideUp();
-    $("#panel-two").slideDown();
-    $("#panel-three").slideUp();
-    $("#panel-four").slideUp();
-  });
-  $("#list-three").click(function(event) {
-    $("#panel-one").slideUp();
-    $("#panel-two").slideUp();
-    $("#panel-three").slideDown();
-    $("#panel-four").slideUp();
-  });
-  $("#list-four").click(function(event) {
-    $("#panel-one").slideUp();
-    $("#panel-two").slideUp();
-    $("#panel-three").slideUp();
-    $("#panel-four").slideDown();
+  $(".list-group-item").click(function() {
+    var index = $(".list-group-item").index(this);
+    $("a").each(function() {
+      if($(".list-group-item").index(this) === index) {
+        $(this).addClass("active");
+      } else {
+        $(this).removeClass("active");
+      }
+    });
+    $(this).addClass("active");
+    $(".panel-body").each(function() {
+      if($(".panel-body").index(this) === index) {
+        $(this).slideDown();
+      } else {
+        $(this).slideUp();
+      }
+    });
   });
 });
