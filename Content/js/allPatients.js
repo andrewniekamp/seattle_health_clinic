d5a3fc1fb51803b5288bfdb9a7071f5a8e55eeac
Andrew Niekamp and Yi Chiang <andrew+yi@>
2016-08-10 11:29:03 -0700

    adjust speed to jquery slidetoggle on patient edit form

--- Content/js/allPatients.js
+++ Content/js/allPatients.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
   $(".patient-edit-button").click(function(){
-    // $(this).closest(".row").next(".patient-update-form").toggle();
-    $(this).parents(".row").next(".form-row").slideToggle();
-
+    $(this).parents(".row").next(".form-row").slideToggle("fast");
   });
 });
